2ed53486a1c01a436efa97a7efe2046e74e4d377
Sergio <sgomezbe@hotmail.com>
2016-08-27 11:36:52 -0500

    commit

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-var express = require("express");
+var express = require('express');
+
+// Create our app
 var app = express();
-app.use(express.static("public"));
-app.listen(3000,function(){
-    console.log("Servidor corriendo en el puerto 3000 ")
-})
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'https') {
+         res.redirect('http://' + req.hostname + req.url);
+        
+    } else { 
+        next();
+    }
+});
+
+app.use(express.static('public'));
+
+app.listen(PORT, function() {
+    console.log('Express server is up on port ' + PORT);
+}); 
