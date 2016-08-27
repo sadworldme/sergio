7b45a39d09106f1fbf66e312d2fea1cc6122dac1
Sergio <sgomezbe@hotmail.com>
2016-08-27 11:54:18 -0500

    hol

--- app/app.jsx
+++ app/app.jsx
@@ -1,3 +1,23 @@
-var React=require("react");
-var ReactDOM=require("react-dom");
-ReactDOM.render(<h1>YANCABRO</h1>,document.getElementById("app"));
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
+var Weather = require('Weather');
+var About = require('About');
+var Examples = require('Examples');
+
+//Load Foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+
+$(document).foundation();
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+      <Route path="about" component={About}/>
+      <Route path="examples" component={Examples}/>
+      <IndexRoute component={Weather}/>
+    </Route>
+  </Router>,
+  document.getElementById('app')
+);
